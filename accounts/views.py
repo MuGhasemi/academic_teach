3b7b85c58b0eca0d336af5aa7747308f329ed458
Muhammad <104085463+MuGhasemi@users.noreply.github.com>
2023-10-05 15:53:30 +0330

    update and fix LoginView

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import redirect, render
 from django.views import View
 from .forms import LoginUserForm
-from .models import Student
-from django.contrib.auth.hashers import check_password
-from django.core.exceptions import ObjectDoesNotExist
-from config.settings import LOGIN_URL
+from django.contrib.auth import authenticate, login
+from config.settings import LOGIN_URL, LOGIN_REDIRECT_URL
 
 class LoginView(View):
     template_name = "accounts/login.html"
@@ -13,22 +11,21 @@ class LoginView(View):
     def get(self, request):
         context = {'form': self.form_class}
         return render(request, self.template_name, context)
-    # TODO - fix login for teachers
+
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            try:
-                _ = Student.objects.get(email=email)
-                valid_password = check_password(password, _.password)
-                if valid_password:
-                    request.session['std_id'] = str(_.student_id)
-                    # TODO - create message for login successfully
-                    return redirect('/')
-                # TODO - create message for login failed
-                return redirect(LOGIN_URL)
-            except ObjectDoesNotExist:
-                _ = None
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                email=cd['email'],
+                                password=cd['password'])
+            if user is not None:
+                login(request, user)
+                # TODO - create message for login successfully
+                if request.GET.get('next'):
+                    return redirect(request.GET.get('next'))
+                return redirect(LOGIN_REDIRECT_URL)
+            # TODO - create message for login failed
         # TODO - create message for login failed
         return redirect(LOGIN_URL)
+

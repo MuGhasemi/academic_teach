311357fba3532292a8d3a103a94d002dceaa0698
Muhammad <104085463+MuGhasemi@users.noreply.github.com>
2023-10-05 15:58:23 +0330

    create RegisterUserForm for RegisterView view

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,3 +16,44 @@ class LoginUserForm(forms.Form):
             attrs={
                 'placeholder': 'رمز ورود',
                 'id':'password',}))
+
+class RegisterUserForm(forms.Form):
+    username = forms.CharField(
+        max_length = 30,
+        required = True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'نام کاربری',
+                'id':'sinUsername',}))
+    first_name = forms.CharField(
+        max_length = 20,
+        required = True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'نام',
+                'id':'sinFirstname',}))
+    last_name = forms.CharField(
+        max_length = 20,
+        required = True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'نام خانوادگی',
+                'id':'sinLastname',}))
+    password = forms.CharField(
+        required = True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'گذرواژه',
+                'id':'sinPassword',}))
+    confirm_password = forms.CharField(
+        required = True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'تکرار گذرواژه',
+                'id':'sinPasswordRep',}))
+    email = forms.CharField(
+        required = True,
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder': 'ایمیل',
+                'id':'sinEmail',}))

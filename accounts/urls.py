5246b060a7b4fcef5f00c7723755893b499dd62b
Muhammad <104085463+MuGhasemi@users.noreply.github.com>
2023-10-05 15:57:11 +0330

    create RegisterView for register student and make url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, RegisterView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('sign-up/', RegisterView.as_view(), name='sign_up'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect, render
 from django.views import View
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
+from .models import User, Student
 from django.contrib.auth import authenticate, login
-from config.settings import LOGIN_URL, LOGIN_REDIRECT_URL
+from config.settings import LOGIN_URL, LOGIN_REDIRECT_URL, SIGN_UP_URL
 
 class LoginView(View):
     template_name = "accounts/login.html"
@@ -29,3 +30,34 @@ class LoginView(View):
         # TODO - create message for login failed
         return redirect(LOGIN_URL)
 
+
+
+class RegisterView(View):
+    template_name = "accounts/register.html"
+    form_class = RegisterUserForm
+
+    def get(self, request):
+        context = {'form': self.form_class}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if  cd['password'] == cd['confirm_password']:
+                user = User.objects.create(
+                    username = cd['username'],
+                    first_name = cd['first_name'],
+                    last_name  = cd['last_name'],
+                    email = cd['email'],
+                    user_type = 'student'
+                    )
+                user.set_password(cd['confirm_password'])
+                user.save()
+                Student.objects.create(user=user, student_image=None)
+                # TODO - create message for register successfully
+                return redirect(LOGIN_URL)
+            # TODO - create message for password does not match
+            return redirect(SIGN_UP_URL)
+        # TODO - create message for register failed
+        return redirect(SIGN_UP_URL)

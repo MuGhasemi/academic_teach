c515b996325d6b8fb748ff193a6b01c7133ee564
Reza <rezammdzd0@gmail.com>
2023-10-14 18:46:11 +0330

    swiper add and slider finished 😎

---
+++ front/assets/App/index.js
@@ -0,0 +1,25 @@
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 30,
+  freeMode: true,
+  slidesPerView: 2.5,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});

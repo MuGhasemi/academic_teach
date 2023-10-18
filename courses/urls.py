c585639f7be55a4c238beeebfc0ffe2e05067f19
Muhammad <104085463+MuGhasemi@users.noreply.github.com>
2023-10-18 20:09:14 +0330

    create LessonDetailView view, add url and create lesson_detail.html template

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'courses'
 urlpatterns = [
-    path('', views.LessonsListView.as_view(), name='home')
+    path('', views.LessonsListView.as_view(), name='home'),
+    re_path(r'lesson/(?P<slug>[-\w]+)/', views.LessonDetailView.as_view(), name='lesson_detail')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Lesson
 from .forms import SearchBoxForm
 from django.db.models import Q
@@ -47,4 +47,17 @@ class LessonsListView(ListView):
             )
             if queryset.count() == 0:
                 print('Not Found!')
+        return queryset
+
+class LessonDetailView(DetailView):
+    model = Lesson
+    template_name = 'courses/lesson_detail.html'
+    context_object_name = 'lesson'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
         return queryset

fdb30b5f8460181ee021909998d9af7681041497
Muhammad <104085463+MuGhasemi@users.noreply.github.com>
2023-10-22 00:01:20 +0330

    create ProfileUser class, delete_photo func, url and profile.html template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import LoginView, RegisterView, LogoutView
+from .views import LoginView, RegisterView,  ProfileUser, LogoutView, delete_photo
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('sign-up/', RegisterView.as_view(), name='sign_up'),
-    path('logout/', LogoutView, name='logout')
+    path('logout/', LogoutView, name='logout'),
+
+    path('profile/', ProfileUser.as_view(), name='profile'),
+    path('profile/delete-photo/', delete_photo, name='delete_photo'),
 ]

b30c5b0f43f8e53c273ebff920a20f7f12d6b666
Reza <rezammdzd0@gmail.com>
2023-10-25 12:48:59 +0330

    nav profile drop down menu

--- front/assets/App/index.js
+++ front/assets/App/index.js
@@ -6,7 +6,7 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 30,
   freeMode: true,
   slidesPerView: 2.5,
-  
+
   // If we need pagination
   // pagination: {
   //   el: '.swiper-pagination',
@@ -24,4 +24,20 @@ const swiper = new Swiper(".swiper", {
   // scrollbar: {
   //   el: '.swiper-scrollbar',
   // },
+});
+
+const navProfile = document.querySelector(".nav__profile");
+const profileDropdown = document.querySelector(".profile__drop-down");
+
+navProfile.addEventListener("mouseenter", () => {
+  profileDropdown.style.display = "block";
+});
+navProfile.addEventListener("mouseleave", () => {
+  profileDropdown.style.display = "none";
+});
+profileDropdown.addEventListener("mouseenter", () => {
+  profileDropdown.style.display = "block";
+});
+profileDropdown.addEventListener("mouseleave", () => {
+  profileDropdown.style.display = "none";
 });

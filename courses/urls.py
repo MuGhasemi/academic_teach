e1d4d7bc62cb9994d396fc1eeb417d40cb345872
Muhammad <104085463+MuGhasemi@users.noreply.github.com>
2023-10-27 17:34:47 +0330

    update url path, user_lessons_list and navigation files because changed class UserLessonsView

--- courses/urls.py
+++ courses/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     re_path(r'lesson/(?P<slug>[-\w]+)/edit/', views.LessonUpdateView.as_view(), name='lesson_update'),
     re_path(r'lesson/(?P<slug>[-\w]+)/', views.LessonDetailView.as_view(), name='lesson_detail'),
     path('add-lesson', views.LessonCreateView.as_view(), name='add_lesson'),
-    path('student/lessons/', views.StudentLessonsView.as_view(), name='student_lesson'),
+    path('user/lessons/', views.UserLessonsView.as_view(), name='user_lesson'),
     path('register-lesson/', views.StudentRegisterLesson.as_view(), name='register-lesson'),
 
 ]

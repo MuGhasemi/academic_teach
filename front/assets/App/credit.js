eee71d9c2585f2e2c46b08e76a3432d94912268f
Muhammad Ghasemi <104085463+MuGhasemi@users.noreply.github.com>
2024-01-22 13:56:34 +0330

    Delete front directory

--- front/assets/App/credit.js
+++
@@ -1,9 +0,0 @@
-const inputValue = document.querySelector("#priceToUp");
-const toShow = document.querySelector("#priceUp");
-const nowCredit = document.querySelector("#nowCredit");
-const priceAfterUp = document.querySelector("#priceAfterUp");
-
-inputValue.addEventListener("input", (e) => {
-  toShow.innerText = e.target.value;
-  priceAfterUp.innerText = +e.target.value + +nowCredit.innerText;
-});
